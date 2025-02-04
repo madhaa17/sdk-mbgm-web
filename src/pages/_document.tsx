9039e1e93cacf2bb656d7879ec48964dfe734139
mikamada <mikamada17@gmail.com>
2025-02-04 14:25:11 +0700

    feat: pwa

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,14 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { ThemeProvider } from "@/components/theme-provider";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="apple-touch-icon" href="/assets/images/logo.png" />
+      </Head>
       <body className="antialiased">
-       
         <Main />
         <NextScript />
       </body>
