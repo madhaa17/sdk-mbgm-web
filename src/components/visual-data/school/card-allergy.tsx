a5b36f1589eea7a61643e0f4076871a8b009bbcf
mikamada <mikamada17@gmail.com>
2025-02-13 17:18:49 +0700

    fix: detail allergy

--- src/components/visual-data/school/card-allergy.tsx
+++ src/components/visual-data/school/card-allergy.tsx
@@ -35,6 +35,8 @@ const CardAllergy = ({ data }: { data: schoolDetail }) => {
     (Number(data?.b_a5_breads_cakes) || 0) +
     (Number(data?.b_a5_food_instant) || 0);
 
+  const vegetarian = Number(data?.b_vegan) || 0;
+
   const calculatePercentage = (value: number) =>
     ((value / totalStudents) * 100).toFixed(2);
 
@@ -42,28 +44,109 @@ const CardAllergy = ({ data }: { data: schoolDetail }) => {
     {
       name: "Alergi Protein Hewani",
       value: calculatePercentage(animalProteinTotal),
+      detail: [
+        {
+          name: "Alergi Telur",
+          value: data.b_a1_egg,
+        },
+        {
+          name: "Alergi Susu",
+          value: data.b_a1_cow_milk,
+        },
+        {
+          name: "Alergi Seafood",
+          value: data.b_a1_seafood,
+        },
+        {
+          name: "Alergi Daging",
+          value: data.b_a1_red_meat,
+        },
+      ],
     },
     {
       name: "Alergi Protein Nabati",
       value: calculatePercentage(plantProteinTotal),
+      detail: [
+        {
+          name: "Alergi Buah",
+          value: data.b_a2_fruit,
+        },
+        {
+          name: "Alergi Latex",
+          value: data.b_a2_latex_fruit,
+        },
+        {
+          name: "Alergi Sayur",
+          value: data.b_a2_vegetable,
+        },
+      ],
     },
     {
       name: "Alergi Rempah",
       value: calculatePercentage(spicesTotal),
+      detail: [
+        {
+          name: "Alergi Kacang-kacangan",
+          value: data.b_a3_sesame,
+        },
+        {
+          name: "Alergi Bumbu",
+          value: data.b_a3_herbs_spices,
+        },
+      ],
     },
     {
       name: "Alergi Tambahan Pangan",
       value: calculatePercentage(additiveTotal),
+      detail: [
+        {
+          name: "Alergi Pewarna Makanan",
+          value: data.b_a4_food_color,
+        },
+        {
+          name: "Alergi Pengawet Makanan",
+          value: data.b_a4_food_preservative,
+        },
+        {
+          name: "Alergi Perasa Buatan",
+          value: data.b_a4_artificial_flavors,
+        },
+      ],
     },
     {
       name: "Alergi Makanan Olahan",
       value: calculatePercentage(processedTotal),
+      detail: [
+        {
+          name: "Alergi Roti & Kue",
+          value: data.b_a5_breads_cakes,
+        },
+        {
+          name: "Alergi Cokelat",
+          value: data.b_a5_chocolate,
+        },
+        {
+          name: "Alergi Makanan Instan",
+          value: data.b_a5_food_instant,
+        },
+      ],
+    },
+
+    {
+      name: "Vegan",
+      value: calculatePercentage(vegetarian),
+      detail: [
+        {
+          name: "Vegan",
+          value: data.b_vegan,
+        },
+      ],
     },
   ];
 
   return (
     <Card className="rounded-2xl bg-card/70 h-full">
-      <CardContent className="p-6 space-y-4">
+      <CardContent className="p-6 space-y-3">
         {categories.map((category, index) => (
           // bar chart
           <div key={index} className="flex items-center justify-between">
@@ -83,10 +166,13 @@ const CardAllergy = ({ data }: { data: schoolDetail }) => {
                     />
                   </div>
                 </TooltipTrigger>
-                <TooltipContent className="z-50">
-                  <p>
-                    {category.value}% dari {totalStudents} siswa {category.name}{" "}
-                  </p>
+                <TooltipContent className="z-50 p-2 shadow-md rounded-md">
+                  <div className="text-sm font-semibold">{category.name}</div>
+                  {category.detail.map((detail, index) => (
+                    <div key={index} className="text-sm">
+                      {detail.name}: {detail.value}
+                    </div>
+                  ))}
                 </TooltipContent>
               </Tooltip>
             </TooltipProvider>
