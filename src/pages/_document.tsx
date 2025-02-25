08095b918869fda356da7973c51d03b3cbffef04
mikamada <mikamada17@gmail.com>
2025-02-25 12:35:30 +0700

    favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,7 @@ export default function Document() {
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#000000" />
         <link rel="apple-touch-icon" href="/assets/images/logo.png" />
+        <link rel="icon" href="/assets/images/logo-2.png" sizes="any" />
       </Head>
       <body className="antialiased">
         <Main />
